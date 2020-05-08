ff991323ede1a5e4056383b4a453146417bb0be1
Soltane <soltanne92@gmail.com>
2020-05-07 23:34:27 -0500

    Cleanup, Navbar & Router Setup

---
+++ client/src/Components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+    <div> 
+        <h1>About this app</h1>
+        <p className="my-1">
+            This a Full Stack React App for Keeping jobs Interview
+        </p>
+        <p className="bg-dark p">
+            <strong>Version: </strong> 1.00
+        </p>
+    </div>
+    );
+}
+ 
+export default About;
